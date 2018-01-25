03cb25ca8db59919bec8fcdf0c9c8460fe6e4bee
web100ry <valerij.ivanjuk@gmail.com>
2018-01-25 14:15:28 +0200

    test Git

--- level2/mod2/file.php
+++ level2/mod2/file.php
@@ -12,7 +12,7 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<html xmlns:test="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
 <head>
 	<title>Работа с файлами</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
@@ -20,20 +20,23 @@
 <body>
 
 <h1>Заполните форму</h1>
-
+<div>
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-
-Имя: <input type="text" name="fname" /><br />
-Фамилия: <input type="text" name="lname" /><br />
-
-<br />
-
-<input type="submit" value="Отправить!" />
-
+      Имя:
+    <input type="text" name="fname"/>
+    <hr>
+Фамилия: <input type="text" name="lname" /><hr>
+<hr>
+<input  type="submit" value="Отправить!" />
 </form>
+
+
+</div>
+    <p>Тест Gita</p>
 
 <?php
 /*
+ *
 ЗАДАНИЕ 2
 - Проверьте, существует ли файл с информацией о пользователях
 - Если файл существует, получите все содержимое файла в виде массива строк
