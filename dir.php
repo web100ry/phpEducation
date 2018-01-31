60763ab9cdc848c8d6be6b3c44253175f43652b9
web100ry <valerij.ivanjuk@gmail.com>
2018-01-31 20:57:32 +0200

    move dir.php to basic catalog

--- dir.php
+++ dir.php
@@ -5,13 +5,22 @@
 Даний коментар э тестовий для перевырки роботи ТУДУ ;)
 */
 
+$files=scandir(".");// другий параметр 1 - зворотнє сортування
+print_r($files);
+foreach ($files as $file){
+    echo $file."<br>";
+}
 
+/*
 $d=opendir(".");
 while ($name=readdir($d)){
+if ($name=="." or $name=="..") continue;
     if(is_dir($name))
         echo "<b>[$name]</b><br>";
     if(is_file($name))
         echo "$name<br>";
 }
 closedir($d);
+*/
+
 ?>
