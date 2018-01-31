d3a17f5f0df1be1d4377593dfc2b2b2328b1739f
web100ry <valerij.ivanjuk@gmail.com>
2018-01-31 21:52:25 +0200

    edit for true directory on dir.php

--- dir.php
+++ dir.php
@@ -4,23 +4,27 @@
 
 Даний коментар э тестовий для перевырки роботи ТУДУ ;)
 */
-
+/*
 $files=scandir(".");// другий параметр 1 - зворотнє сортування
 print_r($files);
 foreach ($files as $file){
     echo $file."<br>";
 }
-
-/*
-$d=opendir(".");
-while ($name=readdir($d)){
-if ($name=="." or $name=="..") continue;
-    if(is_dir($name))
-        echo "<b>[$name]</b><br>";
-    if(is_file($name))
-        echo "$name<br>";
-}
-closedir($d);
 */
-
+function dirs($dir, $tab1)
+{
+    $d = opendir($dir);
+    while ($name = readdir($d)) {
+        $tab=$tab1;
+        if ($name == "." or $name == "..") continue;
+        if (!is_file($name)) {
+            echo "<b>".$tab." [$name]</b><br>";
+            $tab.= "-";
+            dirs($dir."/$name/".".",$tab);
+        } else
+            echo "$tab$name<br>";
+    }
+    closedir($d);
+}
+dirs(".","-");
 ?>
