ed6d40e49407d70b84523f48ec605139732364c3
web100ry <valerij.ivanjuk@gmail.com>
2018-02-10 00:05:50 +0200

    get orders with goods

--- level2/eshop/eshop_lib.inc.php
+++ level2/eshop/eshop_lib.inc.php
@@ -88,31 +88,33 @@ $result=mysqli_query ($connection, $sql) or die(mysqli_error($connection));
 
         }
     }
-	/*
-	ЗАДАНИЕ 6
-	- Опишите функцию resave() для пересохранения товаров из корзины (таблица basket) в заказы (таблица orders)
-	- Функция должна принимать следующие значения:
-			datetime – дата заказа 
-	- Для получения содержимого корзины в этой функции воспользуйтесь функцией myBasket()
-	- Опишите в функции resave() SQL-оператор, который будет вставлять данные из корзины в таблицу orders и выполните его
-	- Опишите SQL-оператор для удаления данных о корзине текущего покупателя из таблицы basket
-	*/
-	
-	/*
-	ЗАДАНИЕ 7
-	- Опишите функцию getOrders() для получения информации о заказах
-	- Получите в виде массива $orders данные о пользователях из файла "orders.log"
-	- Создайте массив $allorders для хранения информации обо всех заказах
-	- В цикле foreach переберите все заказы
-	- Внутри цикла foreach создайте ассоциативный массив $orderinfo для хранения информации о каждом конкретном заказе
-	- Сохраните информацию о пользователе из массива $orders(name, email, phone, address, customer, date) в массиве $orderinfo
-	- Опишите SQL-оператор для выборки из таблицы заказов всех товаров для конкретного покупателя
-	- Получите весь результат этой выборки
-	- Сохраните полученный в предыдущем пункте результат как значение
-		ключа "goods" в массиве $orderinfo
-	- Добавьте сформированный массив $orderinfo в виде значения очередного ключа массива $allorders
-	- Функция getOrders() должна возвращать массив $allorders с информацией о всех покупателях
-		и сделанных ими заказах
-	*/
+
+    function getOrders($connection){
+	    if (!file_exists(ORDERS_LOG))
+	        return false;
+	    $allorders = array();
+	    $orders = file(ORDERS_LOG);
+	    foreach ($orders as $order){
+	        list($n,$e,$p,$a,$c,$dt)= explode("|", $order);
+        $orderinfo=array();
+            $orderinfo["name"] = $n;
+            $orderinfo["email"] = $e;
+            $orderinfo["phone"] = $p;
+            $orderinfo["address"] = $a;
+           $orderinfo["customer"] = $c;
+            $orderinfo["dt"] = $dt*1;
+
+        $sql="SELECT * FROM orders
+              WHERE customer = '$c' 
+              AND datetime = ".$orderinfo["dt"];
+            $result=mysqli_query ($connection, $sql) or die(mysqli_error($connection));
+
+            $orderinfo["goods"] = db2Array($result);
+
+        $allorders[] = $orderinfo;
+	    }
+        return $allorders;
+    }
+
 
 ?>
--- level2/eshop/orders.php
+++ level2/eshop/orders.php
@@ -12,34 +12,65 @@
 <body>
 <h2>Поступившие заказы:</h2>
 <?php
+
+$orders=getOrders($connection);
 /*
-ЗАДАНИЕ 1
-- вызовите функцию getOrders() и сохраните результат её работы
+и сохраните результат её работы
 	в переменную
 - используя цикл foreach выведите информацию обо всех заказах,
 	используя указанную ниже шапку
 */
+foreach ($orders as $order){
+
+
 ?>
 <hr>
-<p><b>Заказчик</b>: </p>
-<p><b>Email</b>: </p>
-<p><b>Телефон</b>: </p>
-<p><b>Адрес доставки</b>: </p>
-<p><b>Дата размещения заказа</b>: </p>
-<h3>Купленные товары:</h3>
+<p><b>Заказчик</b>: <?=$order["name"];?></p>
+<p><b>Email</b>: <?=$order["email"];?> </p>
+<p><b>Телефон</b>: <?=$order["phone"];?> </p>
+<p><b>Адрес доставки</b>: <?=$order["address"];?> </p>
+<p><b>Дата размещения заказа</b>: <?=date("d-m-y H:i:s",$order["dt"]);?> </p>
 <table border="1" cellpadding="5" cellspacing="0" width="90%">
-<tr>
-	<th>N п/п</th>
-	<th>Автор</th>
-	<th>Название</th>
-	<th>Год издания</th>
-	<th>Цена, руб.</th>
-	<th>Количество</th>
-</tr>
+    <tr>
+        <th>N п/п</th>
+        <th>Автор</th>
+        <th>Название</th>
+        <th>Год издания</th>
+        <th>Цена, руб.</th>
+        <th>Количество</th>
+    </tr>
+
+    <?php
+  //  var_dump($order["goods"]);
+    $i = 1;
+    $sum = 0;
+foreach ($order["goods"] as $book) {
+
+        ?>
+
+        <tr>
+            <th><?=$i;?></th>
+            <th><?=$book["author"];?></th>
+            <th><?=$book["title"];?></th>
+            <th><?=$book["pubyear"];?></th>
+            <th><?=$book["price"];?></th>
+            <th><?=$book["quantity"];?></th>
+        </tr>
+
+        <?php
+  $i++;
+  $sum+=$book["price"]*$book["quantity"];
+}
+
+    ?>
+
 
 </table>
-<p>Всего товаров в заказе на сумму: руб.
 
+<p>Всего товаров в заказе на сумму: <?=$sum?> руб.
 
+    <?php
+    }
+?>
 </body>
 </html>
