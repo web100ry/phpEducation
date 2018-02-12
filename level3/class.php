6166e6fb2e120d851ee05061d6e2fcdcba5a1fd6
web100ry <valerij.ivanjuk@gmail.com>
2018-02-12 09:12:46 +0200

    Formating code by Ctrl_Alt_Shift+L

--- level3/class.php
+++ level3/class.php
@@ -15,59 +15,73 @@ $obj2=clone $obj1;//copy for object
 //====================================
 */
 
-class Car{
+class Car
+{
     // властивості (змінні в класі)
-    public $year= "2018"; //рік випіску за умовчанням
+    public $year = "2018"; //рік випіску за умовчанням
     public $speed;
     public $model;
 
-// конструктор класа . Те, що виконуэться автомматично при створені об'єкта
- //function __construct($num)
-     function Car($num)
- {
-     echo "Object $num created! <br>";
- }
+    // конструктор класа . Те, що виконуэться автомматично при створені об'єкта
+    //function __construct($num)
 
- function __destruct()
- {
 
-     //Implement __destruct() method.
- echo "Object DESTRUCT";
- }
+    /**
+     * Car constructor.
+     * @param $num
+     * @return Car
+     */
+    function Car($num)
+    {
+        echo "Object $num created! <br>";
+
+    }
+
+    function __destruct()
+    {
+
+        //Implement __destruct() method.
+        echo "Object DESTRUCT";
+    }
 
     // методи (функція в класі)
 
-    public function getSpeed(){
-        echo "Швидкість = ".$this->speed;
+    public function getSpeed()
+    {
+        echo "Швидкість = " . $this->speed;
     }
 
-    function getClassName(){
+    function getClassName()
+    {
         echo "Class Name: ", __CLASS__;
     }
-    function myMethod(){
-        echo "Method: ",__METHOD__;
+
+    function myMethod()
+    {
+        echo "Method: ", __METHOD__;
     }
+
     function __clone()
     {
         // Implement __clone() method.
-    echo "Object CLONED";
+        echo "Object CLONED";
     }
 }
 
-$car1= new Car(1);
+$car1 = new Car(1);
 $car1->speed = 160;
 $car1->model = "Model One";
-$car2= new Car(2);
+$car2 = new Car(2);
 $car2->speed = 180;
 $car2->model = "Model Two";
 
-echo $car2->getSpeed()."<br>";
+echo $car2->getSpeed() . "<br>";
 
 echo $car1->year;
 echo $car1->speed;
 echo $car1->model;
-echo $car1->myMethod()."<br>";
-echo $car1->getClassName()."<br>";
+echo $car1->myMethod() . "<br>";
+echo $car1->getClassName() . "<br>";
 
 $car3 = clone $car1; //конструктор не викликається
 
--- level3/oop/users.php
+++ level3/oop/users.php
@@ -1,73 +1,109 @@
 <?php
 
-class nameExc extends Exception{
+
+class nameExc extends Exception
+{
+    /**
+     * nameExc constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
     function __construct($message = "", $code = 0, Throwable $previous = null)
     {
-        $message .=" NAME";
+        $message .= " NAME";
         parent::__construct($message, $code, $previous);
     }
 }
 
-class logExc extends Exception{
+class logExc extends Exception
+{
     function __construct($message = "", $code = 0, Throwable $previous = null)
     {
-        $message .=" LOGIN";
+        $message .= " LOGIN";
 
         parent::__construct($message, $code, $previous);
     }
 }
 
-class passExc extends Exception{
+/**
+ * Class passExc
+ */
+class passExc extends Exception
+{
     function __construct($message = "", $code = 0, Throwable $previous = null)
     {
-        $message .=" PASSWORD";
+        $message .= " PASSWORD";
         parent::__construct($message, $code, $previous);
     }
 }
-class User{
+
+/**
+ * Class User
+ */
+class User
+{
     //властивості
-public $name;
-public $login;
-public $password;
+    public $name;
+    public $login;
+    public $password;
 
     // методи
-    public function showInfo(){
 
-    echo "<p>User: ".$this->name."<br>";
-    echo "Login: ".$this->login."<br>";
-    echo "Pass: ".$this->password."<br>";
+    /**
+     * @inheritdoc
+     */
+    public function showInfo()
+    {
+
+        echo "<p>User: " . $this->name . "<br>";
+        echo "Login: " . $this->login . "<br>";
+        echo "Pass: " . $this->password . "<br>";
 
     }
 
-function __construct($name="",$login="",$password="")
-{
-    try{
+    /**
+     * User constructor.
+     *
+     * @param string $name
+     * @param string $login
+     * @param string $password
+     */
+    function __construct($name = "", $login = "", $password = "")
+    {
+        try {
 
-        if ($name == "")
-            throw new nameExc("Введіть: ");
-    $this->name=$name;
-        if ($login == "")
-            throw new logExc("Введіть: ");
-    $this->login=$login;
-        if ($password == "")
-            throw new passExc("Введіть: ");
-    $this->password=$password;
+            if ($name == "")
+                throw new nameExc("Введіть: ");
+            $this->name = $name;
+            if ($login == "")
+                throw new logExc("Введіть: ");
+            $this->login = $login;
+            if ($password == "")
+                throw new passExc("Введіть: ");
+            $this->password = $password;
 
-}
-catch (nameExc $e){echo $e->getMessage();}
-catch (logExc $e){echo $e->getMessage();}
-catch (passExc $e){echo $e->getMessage();}
 
-}
+        } catch (nameExc $e) {
+            echo $e->getMessage();
+        } catch (logExc $e) {
+            echo $e->getMessage();
+        } catch (passExc $e) {
+            echo $e->getMessage();
+        }
 
-function __clone()
-{
-    // Implement __clone() method.
-   $this->name="GUEST";
-   $this->login="guest";
-   $this->password="qwerty";
+        return true;
 
-}
+    }
+
+    function __clone()
+    {
+        // Implement __clone() method.
+        $this->name = "GUEST";
+        $this->login = "guest";
+        $this->password = "qwerty";
+
+    }
 
 }
 
@@ -89,29 +125,30 @@ class superUser extends User
 
     function __construct($name, $login, $password, $role)
     {
-       /*
-        $this->name = $name;
-        $this->login = $login;
-        $this->password = $password;
-       */
-       parent::__construct($name, $login, $password);
-       $this->role = $role;
+        /*
+         $this->name = $name;
+         $this->login = $login;
+         $this->password = $password;
+        */
+        parent::__construct($name, $login, $password);
+        $this->role = $role;
 
     }
 }
-$user5= new superUser("ADMINISTRATOR", "admin", "SuPeRqwerty","SUPERadmin");
 
-     //$user5->role="SUPERadmin";
+$user5 = new superUser("ADMINISTRATOR", "admin", "SuPeRqwerty", "SUPERadmin");
+
+//$user5->role="SUPERadmin";
 
 
-$user1=new User("user1","user11");
+$user1 = new User("user1", "user11");
 /*
     $user1->name="USER1";
     $user1->login="user1";
     $user1->password="user11";
 */
 
-$user2=new User("USER2","user2","user22");
+$user2 = new User("USER2", "user2", "user22");
 
 /*
     $user2->name="USER2";
@@ -119,90 +156,90 @@ $user2=new User("USER2","user2","user22");
     $user2->password="user22";
 */
 
-$user3=new User("USER3","user3","user33");
+$user3 = new User("USER3", "user3", "user33");
 /*
     $user3->name="USER3";
     $user3->login="user3";
     $user3->password="user33";
 */
+
 echo $user1->showInfo();
 echo $user2->showInfo();
 echo $user3->showInfo();
 $user4 = clone $user1;
-$user4->name="Vasya";
+$user4->name = "Vasya";
 echo $user4->showInfo();
 
 echo $user5->showInfo();
 
 
-
- ""
-	/*
-	ЗАДАНИЕ 9
-	- Создайте константу класса User INFO_TITLE
-	- Присвойте константе INFO_TITLE строковое значение "Данные пользователя:"
-	- Обратитесь к константе INFO_TITLE перед вызовами метода showInfo()
-	- Запустите код и проверьте его работоспособность
-	- Создайте метод showTitle() в классе User
-	- Опишите метод showTitle(), чтобы он выводил в браузер значение константы INFO_TITLE
-	- Обратитесь к метод showTitle() перед вызовами метода showInfo()
-	*/
-	/*
-	ЗАДАНИЕ 10
-	- Создайте абстрактный класс AUser
-	- В абстрактном классе AUser объявите абстрактный метод showInfo()
-	- Обновите класс User, унаследовав его от абстрактного класса AUser
-	- Внесите в класс User необходимые изменения
-	- Запустите код и проверьте его работоспособность
-	*/
-	/*
-	ЗАДАНИЕ 11
-	- Создайте интерфейс ISuperUser
-	- В интерфейс ISuperUser объявите метод getInfo()
-	- Опишите метод getInfo() в классе SuperUser
-	- Метод getInfo() должен возвращать ассоциативный массив, в котором 
-	  именами ячеек являются имена свойств объекта, а значениями ячеек - значения свойств объекта
-	- Вызовите метод getInfo() для экземпляра класса SuperUser
-	- В цикле выведите данные, полученные с помощью метода getInfo()
-	*/
-	/*
-	ЗАДАНИЕ 12
-	- Опишите в классах User и SuperUser статические свойства для подсчета количества созданных объектов
-	- Присвойте этим свойствам начальные значения (0)
-	- В конструкторах инкрементируйте значения данных свойств
-	- После создания экземпляров классов User и SuperUser выведите в браузер количество тех и других объектов
-	*/
-	/*
-	ЗАДАНИЕ 13
-	- Опишите функцию checkObject(), которая принимает в качестве входящего параметра объект
-	- Проверьте Ваш объект, используя следующие условия:
-	  Если объект является экземпляром класса SuperUser, выводите сообщение,
-		что данный пользователь обладает правами администратора
-	  Если объект является экземпляром класса User, выводите сообщение,
-		что данный пользователь является обычным пользователем
-	  Если объект не является ни тем, ни другим, выводите сообщение, 
-		что данный пользователь - неизвестный пользователь
-	*/
-	/*
-	ЗАДАНИЕ 14
-	- В директории "oop" создайте файл "User.class.php"
-	- Перенесите описание класса User из файла "users.php" в файл "User.class.php"
-	- В директории "oop" создайте файл "SuperUser.class.php"
-	- Перенесите описание класса SuperUser из файла "users.php" в файл "SuperUser.class.php"
-	- Посторите те же действия для класса AUser и интерфейса ISuperUser
-	- В файле "users.php"(данный файл) опишите функцию __autoload(),
-		которая автоматически присоединяет файлы с описанием классов к файлу "users.php"
-	*/
-	/*
-	ЗАДАНИЕ 15
-	- Создайте свойство objNum, которое будет хранить порядковый номер объекта.
-	  Подумайте, где лучше его создать?
-	- Внесите изменения в классе User (а может и в SuperUser?), которые будут присваивать свойству objNum,
-	  порядковый номер объекта.
-	  Подумайте, где и как лучше это сделать?
-	- В классе User (а может и в SuperUser?) опишите метод __toString()
-	- Данный метод должен возвращать строку в формате "Объект #objNum: name".
-	  Например: "Объект #3: Василий Пупкин"
-	- Попробуйте преобразовать один из созданных Вами объектов в строку
-	*/
+""
+/*
+ЗАДАНИЕ 9
+- Создайте константу класса User INFO_TITLE
+- Присвойте константе INFO_TITLE строковое значение "Данные пользователя:"
+- Обратитесь к константе INFO_TITLE перед вызовами метода showInfo()
+- Запустите код и проверьте его работоспособность
+- Создайте метод showTitle() в классе User
+- Опишите метод showTitle(), чтобы он выводил в браузер значение константы INFO_TITLE
+- Обратитесь к метод showTitle() перед вызовами метода showInfo()
+*/
+/*
+ЗАДАНИЕ 10
+- Создайте абстрактный класс AUser
+- В абстрактном классе AUser объявите абстрактный метод showInfo()
+- Обновите класс User, унаследовав его от абстрактного класса AUser
+- Внесите в класс User необходимые изменения
+- Запустите код и проверьте его работоспособность
+*/
+/*
+ЗАДАНИЕ 11
+- Создайте интерфейс ISuperUser
+- В интерфейс ISuperUser объявите метод getInfo()
+- Опишите метод getInfo() в классе SuperUser
+- Метод getInfo() должен возвращать ассоциативный массив, в котором
+  именами ячеек являются имена свойств объекта, а значениями ячеек - значения свойств объекта
+- Вызовите метод getInfo() для экземпляра класса SuperUser
+- В цикле выведите данные, полученные с помощью метода getInfo()
+*/
+/*
+ЗАДАНИЕ 12
+- Опишите в классах User и SuperUser статические свойства для подсчета количества созданных объектов
+- Присвойте этим свойствам начальные значения (0)
+- В конструкторах инкрементируйте значения данных свойств
+- После создания экземпляров классов User и SuperUser выведите в браузер количество тех и других объектов
+*/
+/*
+ЗАДАНИЕ 13
+- Опишите функцию checkObject(), которая принимает в качестве входящего параметра объект
+- Проверьте Ваш объект, используя следующие условия:
+  Если объект является экземпляром класса SuperUser, выводите сообщение,
+    что данный пользователь обладает правами администратора
+  Если объект является экземпляром класса User, выводите сообщение,
+    что данный пользователь является обычным пользователем
+  Если объект не является ни тем, ни другим, выводите сообщение,
+    что данный пользователь - неизвестный пользователь
+*/
+/*
+ЗАДАНИЕ 14
+- В директории "oop" создайте файл "User.class.php"
+- Перенесите описание класса User из файла "users.php" в файл "User.class.php"
+- В директории "oop" создайте файл "SuperUser.class.php"
+- Перенесите описание класса SuperUser из файла "users.php" в файл "SuperUser.class.php"
+- Посторите те же действия для класса AUser и интерфейса ISuperUser
+- В файле "users.php"(данный файл) опишите функцию __autoload(),
+    которая автоматически присоединяет файлы с описанием классов к файлу "users.php"
+*/
+/*
+ЗАДАНИЕ 15
+- Создайте свойство objNum, которое будет хранить порядковый номер объекта.
+  Подумайте, где лучше его создать?
+- Внесите изменения в классе User (а может и в SuperUser?), которые будут присваивать свойству objNum,
+  порядковый номер объекта.
+  Подумайте, где и как лучше это сделать?
+- В классе User (а может и в SuperUser?) опишите метод __toString()
+- Данный метод должен возвращать строку в формате "Объект #objNum: name".
+  Например: "Объект #3: Василий Пупкин"
+- Попробуйте преобразовать один из созданных Вами объектов в строку
+*/
 ?>
