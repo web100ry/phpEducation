d8c8ddb4b6752f53b4d5035be7a449525506c549
web100ry <valerij.ivanjuk@gmail.com>
2018-02-20 23:29:56 +0200

    XSLT server compare html

--- level3/xml/xslt/xslt.php
+++ level3/xml/xslt/xslt.php
@@ -1,15 +1,17 @@
 <?php
 	//Создание объекта XML
-	
+	$xml=new DOMDocument();
 	//Загрузка XML документа
-	
+	$xml->load("catalog.xml");
 	//Создание объекта XSL
-	
+	$xsl= new  DOMDocument();
 	//Загрузка XSL документа
-	
+	$xsl->load("catalog.xsl");
 	//Создание XSLT парсера
-	
+	$proc = new XSLTProcessor();
 	//Загрузка XSL объекта
-	
+	$proc->importStylesheet($xsl);
 	//Парсинг
+    $html=$proc->transformToXml($xml);
+    echo $html;
 ?>
