6720905a52a2c7e84d5fa13f19b1da8eecae8aa1
web100ry <valerij.ivanjuk@gmail.com>
2018-02-25 13:25:41 +0200

    Stat SOAP from other comp

--- level3/soap/server.php
+++ level3/soap/server.php
@@ -1,5 +1,18 @@
 <?php
-    
+    function getStock($num) {
+        $stock = array (
+            "1"=>100,
+            "2"=>200,
+            "3"=>300,
+            "4"=>400,
+            "5"=>500);
+      if(!array_key_exists($num,$stock))
+            return 0;
+       else
+            return $stock["$num"];
+    }
+
+   echo getStock(4);
 	// Описать функцию/метод Web-сервиса
 	
 	// Отключить кэширование WSDL-документа
