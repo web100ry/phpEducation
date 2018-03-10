65fd591d2dceb1e1f799f74c25fae395f09a335d
web100ry <valerij.ivanjuk@gmail.com>
2018-03-10 21:21:16 +0200

    PDO fetch

--- level4/solution/pdo/sql.php
+++ level4/solution/pdo/sql.php
@@ -1,5 +1,13 @@
 <?php
+class User{
+    public $id;
+    public $email;
+    public $name;
 
+    function nameToUpper () {
+        return strtoupper($this->$name);
+    }
+}
 /**
  * Created by PhpStorm.
  * User: root
@@ -13,15 +21,30 @@ foreach (PDO::getAvailableDrivers()as $driver){
 }
 try{
     $dbh = new PDO($params['db.conn']);
+/*
+    $name="O'Brine";
+    $name= $dbh->quote($name); // екранування спецсимволів
 
-$sql="INSERT INTO users VALUES ('Piter2','654987987')";
+$sql="INSERT INTO users VALUES ($name,'65498321')";
 $result = $dbh->exec($sql);
 echo $result."<br>";//вивід кількість записів з якими були проведені дії
+*/
 
 $sql="SELECT * FROM users";
 foreach ($dbh->query($sql) as $row){
     echo $row['name'].": ".$row['inn']."<br>";
 }
+
+
+$stmt=$dbh->query($sql);
+    $result=$stmt->fetch(PDO::FETCH_ASSOC);
+    foreach ($result as $i=>$k){
+        echo $i.": ".$k."<br>";
+    }
+
+    $result=$stmt->fetch(PDO::FETCH_OBJ);
+        echo $result->name . "<br>";
+
 
 }catch (PDOException $e){
     echo "Ups!".$e->getMessage();
